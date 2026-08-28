59d3cea578af93ee33b95956199f2dff9fc44957
HARSHITHA V <dabohars@gmail.com>
2026-08-28 14:37:01 +0530

    Time: 32 ms (5.58%), Space: 12.3 MB (55.91%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        if(x<0):
+            return False
+
+        original = x
+        reversed=0
+
+        while(x>0):
+            digit = x % 10
+            reversed =  reversed * 10 + digit
+            x=x//10
+
+        return original == reversed 
+
+        
