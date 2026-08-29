9962349662ccfe4eba9f98eadb5d98a7fcb6820b
HARSHITHA V <dabohars@gmail.com>
2026-08-29 12:58:42 +0530

    Time: 7 ms (38.95%), Space: 134.3 MB (25.12%) - LeetHub

---
+++ 2326-spiral-matrix-iv/2326-spiral-matrix-iv.java
@@ -0,0 +1,61 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public int[][] spiralMatrix(int m, int n, ListNode head) {
+
+        int[][] matrix = new int[m][n];
+
+        // Fill everything with -1
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                matrix[i][j] = -1;
+            }
+        }
+
+        int top = 0;
+        int bottom = m - 1;
+        int left = 0;
+        int right = n - 1;
+
+        while (head != null) {
+
+            // Left to right
+            for (int j = left; j <= right && head != null; j++) {
+                matrix[top][j] = head.val;
+                head = head.next;
+            }
+            top++;
+
+            // Top to bottom
+            for (int i = top; i <= bottom && head != null; i++) {
+                matrix[i][right] = head.val;
+                head = head.next;
+            }
+            right--;
+
+            // Right to left
+            for (int j = right; j >= left && head != null; j--) {
+                matrix[bottom][j] = head.val;
+                head = head.next;
+            }
+            bottom--;
+
+            // Bottom to top
+            for (int i = bottom; i >= top && head != null; i--) {
+                matrix[i][left] = head.val;
+                head = head.next;
+            }
+            left++;
+        }
+
+        return matrix;
+    }
+}
